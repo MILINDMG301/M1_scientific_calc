ed1e81ffeedb40a84a99f1ed5dd51b47d3ef93de
MILINDMG301 <milindgavade301@gmail.com>
2022-04-03 17:10:11 +0530

    add output images

--- 3_Implementation/inc/calc_header.h
+++ 3_Implementation/inc/calc_header.h
@@ -14,8 +14,9 @@
 /**
  * @brief Funtion to find the sum of two float or int numbers
  * 
- * @param[in] number1
- * @param[in] number2 
+ * @param[in] number1 to add
+ * @param[in] number2 to add
+ * @param[out] result result of sum operation
  * @return float result of the sum operation
  */
 float addition(float number1, float number2);
@@ -24,6 +25,7 @@ float addition(float number1, float number2);
  * @brief Function to find square of a float number
  * 
  * @param[in] number for which the square has to be found
+ * @param[out] result result of square operation
  * @return float Result of the square operation
  */
 float square(float number);
@@ -33,6 +35,7 @@ float square(float number);
  * 
  * @param[in] number1
  * @param[in] number2 
+ * @param[out] result result of substract operation
  * @return float result of the difference operation
  */
 float substract(float number1, float number2);
@@ -42,6 +45,7 @@ float substract(float number1, float number2);
  * 
  * @param[in] number1
  * @param[in] number2 
+ * @param[out] result result of product operation
  * @return float result of the product operation 
  */
 float prod(float number1, float number2);
@@ -51,6 +55,7 @@ float prod(float number1, float number2);
  * 
  * @param[in] dividend
  * @param[in] divisor 
+ * @param[out] result result of division operation
  * @return float result of the division operation
  */
 float division(float dividend, float divisor);
@@ -60,6 +65,7 @@ float division(float dividend, float divisor);
  * 
  * @param[in] dividend
  * @param[in] divisor
+ * @param[out] result result of remainder operation
  * @return int remainder of the division operation
  */
 int remaind(int number1, int number2);
@@ -68,6 +74,7 @@ int remaind(int number1, int number2);
  * @brief Funtion to find the mod of integer or float number 
  * 
  * @param[in] number for which mod has to be found
+ * @param[out] result result of mod operation
  * @return float result of the mod operation
  */
 float mod(float number);
@@ -76,6 +83,7 @@ float mod(float number);
  * @brief Funtion to find the factorial of integer number
  * 
  * @param[in] number to find factorial
+ * @param[out] result result of factorial operation
  * @return int result of the factorial operation
  */
 int facto(int number);
@@ -85,6 +93,7 @@ int facto(int number);
  * 
  * @param[in] numbers series
  * @param[in] its size of the series
+ * @param[out] result result of summation operation
  * @return int result of the summation operation on the series
  */
 float summation(float series[],int size);
@@ -94,6 +103,7 @@ float summation(float series[],int size);
  * 
  * @param[in] series of numbers 
  * @param[in] its size of the series
+ * @param[out] result resultant series of squared operation
  * @return float result as squared series of the given series
  */
 float * sq(float series[],int size);
@@ -103,6 +113,7 @@ float * sq(float series[],int size);
  * 
  * @param[in] series of numbers
  * @param[in] its size of the series
+ * @param[out] result sum of squared operation on series
  * @return float result of the sum operation of squared series
  */
 float sqsum(float series[],int size);
@@ -113,6 +124,7 @@ float sqsum(float series[],int size);
  * @param[in] number1 series
  * @param[in] number2 series
  * @param[in] its size of the series
+ * @param[out] result sum of product operation of both series
  * @return float result of the sum operation on the product series of both series
  */
 float sumxy(float seriesx[], float seriesy[],int size);
@@ -123,6 +135,7 @@ float sumxy(float seriesx[], float seriesy[],int size);
  * @param[in] number1 series which is cached and squared series
  * @param[in] number2 series
  * @param[in] its size of the series
+ * @param[out] result sum of product operation on one squared series and another series
  * @return float result of the sum operation on the product series of both series 
  */
 float xsqy(float seriesx[], float seriesy[],int size);
@@ -132,6 +145,7 @@ float xsqy(float seriesx[], float seriesy[],int size);
  * 
  * @param[in] series of numbers
  * @param[in] its size of the series
+ * @param[out] result sum of cube operation on the series
  * @return float result of the sum operation of cube series
  */
 float cubx(float series[],int size);
@@ -141,6 +155,7 @@ float cubx(float series[],int size);
  * 
  * @param[in] series of numbers
  * @param[in] its size of the series
+ * @param[out] result mean of the given series
  * @return float result of the mean operation of series
  */
 float meanseries(float series[],int size);
@@ -150,6 +165,7 @@ float meanseries(float series[],int size);
  * 
  * @param[in] series of numbers
  * @param[in] its size of the series
+ * @param[out] result median of the given series
  * @return float result of the median operation of series
  */
 float medianseries(float series[],int size);
@@ -158,6 +174,7 @@ float medianseries(float series[],int size);
  * @brief Function to find sine of the angle
  * 
  * @param[in] angle in degrees
+ * @param[out] result result of sine operation on angle
  * @return float Result of the sine operation
  */
 float sinx(float angle);
@@ -166,6 +183,7 @@ float sinx(float angle);
  * @brief Function to find cos of the angle
  * 
  * @param[in] angle in degrees
+ * @param[out] result result of cos operation on angle
  * @return float Result of the cos operation
  */
 float cosx(float angle);
@@ -174,6 +192,7 @@ float cosx(float angle);
  * @brief Function to find tan of the angle
  * 
  * @param[in] angle in degrees
+ * @param[out] result result of tan operation on angle
  * @return float Result of the tan operation
  */
 float tanx(float angle);
@@ -182,6 +201,7 @@ float tanx(float angle);
  * @brief Function to find cosec of the angle
  * 
  * @param[in] angle in degrees
+ * @param[out] result result of cosec operation on angle
  * @return float Result of the cosec operation
  */
 float cosecx(float angle);
@@ -190,6 +210,7 @@ float cosecx(float angle);
  * @brief Function to find sec of the angle
  * 
  * @param[in] angle in degrees
+ * @param[out] result result of sec operation on angle
  * @return float Result of the sec operation
  */
 float secx(float angle);
@@ -198,6 +219,7 @@ float secx(float angle);
  * @brief Function to find cot of the angle
  * 
  * @param[in] angle in degrees
+ * @param[out] result result of cot operation on angle
  * @return float Result of the cot operation
  */
 float cotx(float angle);
